9070f1c19a3d3129c3c80ef91be8c7725c6aea81
wylyon <willis.lyon@yahoo.com>
2024-07-03 14:08:16 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,14 +17,21 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+  
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
+        {todos.map(todo => <li
+          onClick={() => deleteTodo(todo.id)}
+          key={todo.id}>
+          {todo.content}
+        </li>)}                
       </ul>
       <div>
         🥳 App successfully hosted. Try creating a new todo.
