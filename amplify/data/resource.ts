f28d7cecbc212cddde0e85a49554a946a0d8f578
Willis Lyon <willislyon@Williss-MacBook-Pro-2.local>
2024-07-24 15:05:45 -0700

    Enable mysql.

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,6 +3,9 @@ import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 /*== STEP 1 ===============================================================
 The section below creates a Settings DB table for global app settings.
 =========================================================================*/
+import { schema as generatedSqlSchema } from './schema.sql';
+const sqlSchema = generatedSqlSchema.authorization(allow => allow.guest())
+
 const schema = a.schema({
   Settings: a
     .model({
@@ -12,10 +15,12 @@ const schema = a.schema({
     .authorization((allow) => [allow.owner()]),
 });
 
-export type Schema = ClientSchema<typeof schema>;
+const combinedSchema = a.combine([schema, sqlSchema]);
+
+export type Schema = ClientSchema<typeof combinedSchema>;
 
 export const data = defineData({
-  schema,
+    schema: combinedSchema,
   authorizationModes: {
     defaultAuthorizationMode: "userPool",
   },
