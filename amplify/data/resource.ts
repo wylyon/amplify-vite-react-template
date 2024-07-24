d1677c22570085a822cb96f7b925483a0f098c12
Willis Lyon <willislyon@Williss-MacBook-Pro-2.local>
2024-07-24 15:49:38 -0700

    Enable mysql fix.

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,8 +1,5 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
-/*== STEP 1 ===============================================================
-The section below creates a Settings DB table for global app settings.
-=========================================================================*/
 import { schema as generatedSqlSchema } from './schema.sql';
 const sqlSchema = generatedSqlSchema.authorization(allow => allow.guest())
 
@@ -16,12 +13,10 @@ const schema = a.schema({
 });
 
 const combinedSchema = a.combine([schema, sqlSchema]);
-
 export type Schema = ClientSchema<typeof combinedSchema>;
-
 export const data = defineData({
     schema: combinedSchema,
-  authorizationModes: {
+    authorizationModes: {
     defaultAuthorizationMode: "userPool",
   },
 });
