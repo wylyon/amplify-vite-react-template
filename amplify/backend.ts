1db05bb1336ded722e72855ed6aa4f7c630f8b21
Willis Lyon <willislyon@Williss-MacBook-Pro-2.local>
2024-08-15 08:09:56 -0700

    Restore data to backend.

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,8 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-//import { data } from './data/resource';
+import { data } from './data/resource';
 
 defineBackend({
-  auth
+  auth,
+  data
 });
