969c879c2940ca818c4918e6b44868fb019a6d6f
Will Lyon <willis.lyon@yahoo.com>
2024-09-17 12:26:43 -0700

    Turn off Prod Data feeds

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,8 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { data } from './data/resource';
 
 defineBackend({
-  auth,
-  data
+  auth
 });
