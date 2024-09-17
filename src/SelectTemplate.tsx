819e165c83e427340bda66d9cfaba90cf11e5486
Will Lyon <willis.lyon@yahoo.com>
2024-09-17 14:00:53 -0700

    Fix of build and SelectTemplate failure.

--- src/SelectTemplate.tsx
+++ src/SelectTemplate.tsx
@@ -16,10 +16,7 @@ export default function SelectTemplate(props) {
       <label htmlFor="templateToUse"><b>Program:</b></label>
 	    <select name="templateToUse" id="templateToUse" onChange={handleSelectChange}> 
 	      <option key="0" value=''></option>
-        props.userItems.map(comp => )
-	  {division != null && division.map(comp => (!comp.deactive_date) ? 
-	    (<option key={comp.name + "|" + comp.id} value={comp.name + "|" + comp.id}>{comp.name}</option>) : null)}
-	</select>
+	    </select>
     </div>
   );
 }
