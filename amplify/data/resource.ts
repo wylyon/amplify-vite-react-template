a85fe81e255022de0bbd4d7e92ca1bf47d02ec32
Will Lyon <willis.lyon@yahoo.com>
2024-09-30 10:28:55 -0700

    Added orderby and additional auth

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,7 +1,9 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 import { schema as generatedSqlSchema } from './schema.sql';
-const sqlSchema = generatedSqlSchema.authorization(allow => allow.publicApiKey())
+const sqlSchema = generatedSqlSchema.authorization(allow => [
+  allow.publicApiKey(),
+  allow.owner()])
   .addQueries({
     listUserByEmail: a.query()
     .arguments({
@@ -73,7 +75,8 @@ const sqlSchema = generatedSqlSchema.authorization(allow => allow.publicApiKey()
     })
     .returns(a.ref("template_question").array())
     .handler(a.handler.inlineSql(
-      "SELECT id, template_id, question_order, pre_load_attributes, title, description, question_type, question_values, post_load_attributes, optional_flag, actions_flag, notes, created, created_by FROM logistics.template_question WHERE template_id = :templateId;"
+      "SELECT id, template_id, question_order, pre_load_attributes, title, description, question_type, question_values, post_load_attributes, optional_flag, actions_flag, notes, created, created_by FROM " +
+      "logistics.template_question WHERE template_id = :templateId order by question_order;"
     )).authorization(allow => allow.publicApiKey()),
   })
 
