2e79907900cc37ae2ef01b33a152aa040e044166
Will Lyon <willis.lyon@yahoo.com>
2024-09-30 10:35:11 -0700

    Rollback on auth

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,9 +1,7 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 import { schema as generatedSqlSchema } from './schema.sql';
-const sqlSchema = generatedSqlSchema.authorization(allow => [
-  allow.publicApiKey(),
-  allow.owner()])
+const sqlSchema = generatedSqlSchema.authorization(allow => allow.publicApiKey())
   .addQueries({
     listUserByEmail: a.query()
     .arguments({
