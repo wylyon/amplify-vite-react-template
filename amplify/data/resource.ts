cb30c2b9be74b3c5e5a52b8f55425c8b75684a57
Will Lyon <willis.lyon@yahoo.com>
2024-09-30 12:22:26 -0700

    Auth change back

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,7 +1,7 @@
 import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
 
 import { schema as generatedSqlSchema } from './schema.sql';
-const sqlSchema = generatedSqlSchema.authorization(allow => allow.authenticated('identityPool'))
+const sqlSchema = generatedSqlSchema.authorization(allow => allow.publicApiKey())
   .addQueries({
     listUserByEmail: a.query()
     .arguments({
@@ -75,6 +75,14 @@ const sqlSchema = generatedSqlSchema.authorization(allow => allow.authenticated(
     .handler(a.handler.inlineSql(
       "SELECT id, template_id, question_order, pre_load_attributes, title, description, question_type, question_values, post_load_attributes, optional_flag, actions_flag, notes, created, created_by FROM " +
       "logistics.template_question WHERE template_id = :templateId order by question_order;"
+    )).authorization(allow => allow.publicApiKey()),
+    deleteQuestionById: a.query()
+    .arguments({
+      questionId: a.string().required(),
+    })
+    .returns(a.json().array())
+    .handler(a.handler.inlineSql(
+      "DELETE from logistics.template_question WHERE id = :questionId;"
     )).authorization(allow => allow.publicApiKey()),
   })
 
