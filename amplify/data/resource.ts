d4ccf384ad9adb26a36a536ae1ace069539793d2
Will Lyon <willis.lyon@yahoo.com>
2024-10-22 11:44:50 -0700

    Added trigger_value to view

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -91,7 +91,8 @@ const sqlSchema = generatedSqlSchema.authorization(allow => allow.publicApiKey()
     })
     .returns(a.ref("template_question").array())
     .handler(a.handler.inlineSql(
-      "SELECT id, template_id, question_order, pre_load_attributes, title, description, question_type, question_values, post_load_attributes, optional_flag, actions_flag, notes, created, created_by FROM " +
+      "SELECT id, template_id, question_order, pre_load_attributes, title, description, question_type, question_values, post_load_attributes, optional_flag, " +
+      "actions_flag, notes, created, created_by, trigger_value FROM " +
       "logistics.template_question WHERE template_id = :templateId and deactive_date is null order by question_order;"
     )).authorization(allow => allow.publicApiKey()),
   }).addToSchema({
