ec2c4eca95abe3135573e05d006aa6ee7fc9d8cd
Will Lyon <willis.lyon@yahoo.com>
2025-01-14 11:55:40 -0800

    Fix of verificationBody

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -9,9 +9,8 @@ export const auth = defineAuth({
     email: {
       verificationEmailStyle: "LINK",
       verificationEmailSubject: "Invitation to Join LogIt Pro App",
-      verificationEmailBody: (createLink) => 
-        "You've been invited to join the LogIt Pro logging app by your admin and prompted to accept the invitation and create your account using the provided link: ${createLink('LogIt Pro Logging App')} .\n" +
-        "Note:\n\nIf you have any questions, reach out to your company's Admin.\nAccept the invitation to join the logging app and create your account using the provided email address and Google SSO.",
+      verificationEmailBody: (createLink) => `You have been invited to join the LogIt Pro logging app by your admin and prompted to accept the invitation and create your account using the provided link: ${createLink()}.` +
+        "\nNote:\n\nIf you have any questions, reach out to your company's Admin.\nAccept the invitation to join the logging app and create your account using the provided email address and Google SSO.",
     },
     externalProviders: {
       google: {
