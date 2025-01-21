b5dd2e4f13a2790d2f207268e0f5edc5c93f8963
Will Lyon <willis.lyon@yahoo.com>
2025-01-21 10:31:25 -0800

    Refine function return

--- amplify/functions/get-creds/handler.ts
+++ amplify/functions/get-creds/handler.ts
@@ -1,12 +1,9 @@
 import { env } from '$amplify/env/get-creds';
 
 export const handler = async () => {
-  const request = new Request(env.NAME, {
-    headers: {
-      AccessKeyId: `${env.API_ACCESS_KEY_ID}`,
-      SecretAccess: `${env.API_SECRET_KEY}`,
-      Region: `${env.API_REGION}`,
-    }
-  })
-  return `${env.NAME}`;
+  return {
+    k: `${env.API_ACCESS_KEY_ID}`,
+    s: `${env.API_SECRET_KEY}`,
+    r: `${env.API_REGION}`,
+  }
 };
