dfeb76d283a9d710e8e6a428da014c46595bd384
Will Lyon <willis.lyon@yahoo.com>
2025-02-05 10:13:44 -0800

    Redeploy

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,11 +2,9 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { storage } from './storage/resource';
-import { getCreds } from './functions/get-creds/resource';
 
 defineBackend({
   auth,
   data,
-  storage,
-  getCreds
+  storage
 });
