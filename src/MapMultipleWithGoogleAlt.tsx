4f7df13e05ac6c98f7aa7aa26e86e4ef2b4c6a95
Will Lyon <willis.lyon@yahoo.com>
2025-05-13 11:28:28 -0700

    Added more directions..including waypoints.

--- src/MapMultipleWithGoogleAlt.tsx
+++ src/MapMultipleWithGoogleAlt.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
-import { useState} from "react";
+import { useState, useRef, useCallback} from "react";
 import { StorageImage } from '@aws-amplify/ui-react-storage';
 import { downloadData } from 'aws-amplify/storage';
 import Box  from '@mui/material/Box';
 import moment from 'moment';
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import Tooltip from '@mui/material/Tooltip';
 import { Button } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
 import { Stack } from '@mui/material';
 import TextField from '@mui/material/TextField';
+import NearMeIcon from '@mui/icons-material/NearMe';
 import { GoogleMap, useJsApiLoader, Marker, InfoWindow, DirectionsRenderer } from '@react-google-maps/api'
 
 const containerStyle = {
@@ -48,18 +51,48 @@ export default function MapMultipleWithGoogleAlt(props) {
     );
   };
   
+  const handleGetDirectionsAll = () => {
+    const directionsService = new window.google.maps.DirectionsService();
+    directionsService.route(
+      {
+        origin: {
+          lat: props.markers[0].lattitude,
+          lng: props.markers[0].longitude,
+        },
+        destination: {
+          lat: props.markers[props.markers.length - 1].lattitude,
+          lng: props.markers[props.markers.length - 1].longitude,
+        },
+        waypoints: props.markers.slice(1, -1).map((location) => ({
+          location: { lat: location.lattitude,
+              lng: location.longitude },
+          stopover: true,
+        })),
+        travelMode: window.google.maps.TravelMode.DRIVING,
+      },
+      (response, status) => {
+        if (status === 'OK') {
+          setDirections(response);
+        } else {
+          console.error(`Directions request failed due to ${status}`);
+        }
+      }
+    );
+  };
+
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: props.googleAPI
   })
 
-  const [map, setMap] = React.useState(null)
+  const [map, setMap] = useState(/** @type google.maps.Map */ (null));
+  const mapRef = useRef(null);
 
   const onLoad = React.useCallback(function callback(map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center)
     //map.fitBounds(bounds)
-
+    mapRef.current = map;
     setMap(map)
   }, [])
 
@@ -83,20 +116,6 @@ export default function MapMultipleWithGoogleAlt(props) {
 		}
   }
 
-  const handleDirectionOrigin = () => {
-    setOrigin({
-      lat: selectedCenter.lattitude,
-      lng: selectedCenter.longitude
-    });
-  }
-
-  const handleDirectionDestination = () => {
-    setDestination({
-      lat: selectedCenter.lattitude,
-      lng: selectedCenter.longitude
-    });
-  }
-
   const handleAlignment = (
     event: React.MouseEvent<HTMLElement>,
     newAlignment: string | null,
@@ -115,7 +134,7 @@ export default function MapMultipleWithGoogleAlt(props) {
     if (alignment) {
       if (alignment == 'origin') {
         setOrigin(latLng);
-      } else {
+      } else if (alignment == 'destination') {
         setDestination(latLng);
       }
     }
@@ -205,7 +224,9 @@ export default function MapMultipleWithGoogleAlt(props) {
     <div>
       <Stack direction='column' spacing={2}>
         <Stack direction='row'>
-          <Button variant='contained' color='info' onClick={handleGetDirections}>Get Directions</Button><Button variant='contained' color='error' onClick={resetDirections}>Reset</Button>
+          <Button variant='contained' color='info' disabled={!origin || !destination} onClick={handleGetDirections}>Get Directions</Button>
+          <Button variant='contained' color='info' disabled={props.markers.length < 2} onClick={handleGetDirectionsAll}>Directions All Markers</Button>
+          <Button variant='contained' color='error' onClick={resetDirections}>Reset</Button>
           <ToggleButtonGroup
             value={alignment}
             exclusive
@@ -217,12 +238,30 @@ export default function MapMultipleWithGoogleAlt(props) {
           </ToggleButtonGroup>
         </Stack>
         <Stack direction='row'>
-          {origin ? <TextField id="origin-text" label="Origin" variant='outlined'
-          value={origin ? typeof origin === 'object' ? origin.lat + ',' + origin.lng : origin : ''} 
-            onChange={(e) => setOrigin(e.target.value)} /> : null }
-          {destination ? <TextField id="destination-text" label="Destination" variant='outlined'
-            value={destination ? typeof destination === 'object' ? destination.lat + ',' + destination.lng : destination : ''} 
-            onChange={(e) => setDestination(e.target.value)} /> : null}
+          {origin || alignment == 'manual' ? 
+          <Stack direction='row'>
+              <TextField id="origin-text" label="Origin" variant='outlined'
+              value={origin ? typeof origin === 'object' ? origin.lat + ',' + origin.lng : origin : ''} 
+              onChange={(e) => setOrigin(e.target.value)} 
+              /> 
+            {origin ?
+            <Tooltip title='Pan to origin location'>
+              <IconButton aria-label="origin-loc" onClick={(e) => mapRef.current.panTo(origin)}><NearMeIcon /></IconButton>
+            </Tooltip> : null}
+          </Stack>
+            : null }
+          {destination || alignment == 'manual' ? 
+          <Stack direction='row'>
+              <TextField id="destination-text" label="Destination" variant='outlined'
+                value={destination ? typeof destination === 'object' ? destination.lat + ',' + destination.lng : destination : ''} 
+                onChange={(e) => setDestination(e.target.value)} 
+              /> 
+            {destination ?
+            <Tooltip title='Pan to destination location'>
+              <IconButton aria-label="origin-loc" onClick={(e) => mapRef.current.panTo(destination)}><NearMeIcon /></IconButton>
+            </Tooltip> : null}
+          </Stack>
+          : null}
         </Stack>
       </Stack>
       {directions && (
